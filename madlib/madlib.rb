d6d133e7213424c928972478d9760ac5fbdf2de0
Brad Diver <braddiver@gmail.com>
2012-01-19 20:23:08 -0500

    Added colors for answers

--- madlib/madlib.rb
+++ madlib/madlib.rb
@@ -1,13 +1,18 @@
 require 'rubygems'
 require 'highline/import'
 
-def colorize(text, color_code)
-  "#{color_code}#{text}e[0m"
+
+def hilite(text)
+  #http://pueblo.sourceforge.net/doc/manual/ansi_color_codes.html
+  blue = '34'
+  green = '32'
+  red = '31'
+  #http://highline.rubyforge.org/doc/classes/HighLine.html
+  return "\e["+blue+"m"+text+"\e[0m"
 end
-def green(text); colorize(text, "e[32m"); end
 
 questions=[
-green("Give me a VERB: "),
+"Give me a VERB: ",
 "Give me an ADJECTIVE: ",
 "Give me a NOUN (PLURAL): ",
 "Give me an ADJECTIVE: ",
@@ -30,8 +35,7 @@ say "\n\n\n\n\n\n\n\n\n\n\n\n\n\n##########################################\n###
 
 
 questions.length.times {|i| 
-  answers[i] = ask(questions[i]) do |response0| 
-  end
+  answers[i] = ask(questions[i])
 }
  
 
@@ -39,7 +43,7 @@ questions.length.times {|i|
   if answers[i] == ""
     answers[i] = "(no answer given)" 
   end
-  answers[i] = green(answers[i])
+  answers[i] = hilite(answers[i])
 }
 
 madlibText = "Come #{answers[0]} at Trevor's Ruby Tuesday Class, where you`ll receive #{answers[1]} information on all of your favorite  #{answers[2]}. Our #{answers[3]} and #{answers[4]} coworkers are there to #{answers[5]} you #{answers[6]} hours a day. Here you will find #{answers[7]} answers to any #{answers[8]} questions you may have. We have #{answers[9]} for the front end geeks, #{answers[10]} for the middleware nerds and all the latest what-nots for your aunt #{answers[11]}. So come on down to your #{answers[12]} #{answers[13]} classroom where the #{answers[14]} come first."
